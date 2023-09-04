333dee8776dd326f5295d4e3c68a80feae810b3e
parsaM110 <pmomeni80@gmail.com>
2023-09-04 09:14:03 +0330

    'some more info over string & val'

--- src/main/kotlin/com/example/gitircource/basics/Variables.kt
+++ src/main/kotlin/com/example/gitircource/basics/Variables.kt
@@ -2,15 +2,31 @@ package com.example.gitircource.basics
 
 fun main(){
 
-    val name : String = "Dilip"
+    val name = "Dilip"
     println(name)
 
-    var age : Int = 34
+    var age = 34
     println(age)
     age = 35
     println(age)
 
 
-    val salary : Long = 3000
+    val salary = 3000L
     println(salary)
+
+    val course = "Kotlin Spring"
+    println("course: $course and the course length is ${course.length}") // string interpolation | string template
+
+    val multiLine = "ABC \n DEF"
+    println(multiLine)
+
+    val multiLine1 = """ 
+        ABC
+        DEF
+    """.trimIndent()
+
+    print(multiLine1)
+
+
+
 }
