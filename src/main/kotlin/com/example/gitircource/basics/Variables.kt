40d5e086846338716c510491c21af74caa1a0a70
parsaM110 <pmomeni80@gmail.com>
2023-09-04 09:05:36 +0330

    'var & val'

---
+++ src/main/kotlin/com/example/gitircource/basics/Variables.kt
@@ -0,0 +1,16 @@
+package com.example.gitircource.basics
+
+fun main(){
+
+    val name : String = "Dilip"
+    println(name)
+
+    var age : Int = 34
+    println(age)
+    age = 35
+    println(age)
+
+
+    val salary : Long = 3000
+    println(salary)
+}
