f580e843be0a976e444670caba1a3e83fec57d8f
parsaM110 <pmomeni80@gmail.com>
2023-09-04 21:19:40 +0330

    'while and dowhile'

--- src/main/kotlin/com/example/gitircource/basics/LoopsAndRanges.kt
+++ src/main/kotlin/com/example/gitircource/basics/LoopsAndRanges.kt
@@ -1,25 +1,47 @@
 package com.example.gitircource.basics
 
 fun main(){
-    val range=1..10
+//    val range=1..10
+//
+//    for (i in range){
+//        println("i : $i")
+//    }
+//
+//    println("<<<<<<<<<<<<<<<<<<<<<<<<<<<*>>>>>>>>>>>>>>>>>>>>>>>>>>>")
+//
+//
+//    val  reverseRange = 10 downTo 1
+//
+//    for (i in reverseRange){
+//        println("reverseRange : $i")
+//    }
+//
+//    println("<<<<<<<<<<<<<<<<<<<<<<<<<<<*>>>>>>>>>>>>>>>>>>>>>>>>>>>")
+//
+//    for (i in reverseRange step 2){
+//        println("reverseRange with skip : $i")
+//    }
 
-    for (i in range){
-        println("i : $i")
+    explorewhile()
+    println("<<<<<<<<<<<<<<<<<<<<<*>>>>>>>>>>>>>>>>>>>>>")
+    exploreDoWhile()
+
+}
+
+fun exploreDoWhile() {
+    var i = 0
+    do{
+        println("Value of i is : $i")
+        i++
+    }while(i < 5)
+
+}
+
+fun explorewhile() {
+   var x = 1
+
+    while(x<5){
+        println("Value of x is : $x")
+        x++
     }
-
-    println("<<<<<<<<<<<<<<<<<<<<<<<<<<<*>>>>>>>>>>>>>>>>>>>>>>>>>>>")
-
-
-    val  reverseRange = 10 downTo 1
-
-    for (i in reverseRange){
-        println("reverseRange : $i")
-    }
-
-    println("<<<<<<<<<<<<<<<<<<<<<<<<<<<*>>>>>>>>>>>>>>>>>>>>>>>>>>>")
-
-    for (i in reverseRange step 2){
-        println("reverseRange with skip : $i")
-    }
-
 }
