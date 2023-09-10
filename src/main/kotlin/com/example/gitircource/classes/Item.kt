1da6e71715a3b43960961e88a710b00cd6b22dc0
parsaM110 <pmomeni80@gmail.com>
2023-09-10 15:53:38 +0330

    'using setter and getters'

--- src/main/kotlin/com/example/gitircource/classes/Item.kt
+++ src/main/kotlin/com/example/gitircource/classes/Item.kt
@@ -1,19 +1,37 @@
 package com.example.gitircource.classes
 
 class Item() {
-    var name: String  = ""
+    var name: String = ""
+    var price: Double = 0.0
+        //        get() = field
+        get() {
+            println("Inside Getter")
+            return field
+        }
+        set(value) {
+            if(value>=0.0){
+                println("Inside Setter")
+                field = value
+            }
+            else{
+                throw IllegalArgumentException("Negetive price is not allowed")
+            }
+        }
 
-    constructor(_name: String) : this(){
+    constructor(_name: String) : this() {
         name = _name
     }
 
 }
 
 fun main() {
+
     val item = Item("Iphone")
     println("Item is ${item.name}")
     item.name = "Iphone 13"
     println("Item is ${item.name}")
 
-    println(item)
+    item.price = -10.0
+
+    println(item.price)
 }
