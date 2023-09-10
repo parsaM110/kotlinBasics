02d01e869293d595195f60f407864d74701d0232
parsaM110 <pmomeni80@gmail.com>
2023-09-10 12:40:13 +0330

    'init in class'

--- src/main/kotlin/com/example/gitircource/classes/Person.kt
+++ src/main/kotlin/com/example/gitircource/classes/Person.kt
@@ -3,6 +3,13 @@ package com.example.gitircource.classes
 class Person(val name: String = "", val age: Int = 0) {
 
     var email: String = ""
+    var nameLength: Int = 0
+
+    init {
+        println("Inside Init Block")
+        nameLength = name.length
+
+    }
 
     constructor(_email: String, _name: String = "", _age: Int) : this() {
         email = _email
@@ -15,18 +22,18 @@ class Person(val name: String = "", val age: Int = 0) {
 }
 
 fun main() {
-    val person = Person("Alex", 25)
-    person.action()
-    println("Name : ${person.name} and Age: ${person.age}")
-
-    val person1 = Person()
-    person.action()
-    println("Name : ${person1.name} and Age: ${person1.age}")
+//    val person = Person("Alex", 25)
+//    person.action()
+//    println("Name : ${person.name} and Age: ${person.age}")
+//
+//    val person1 = Person()
+//    person.action()
+//    println("Name : ${person1.name} and Age: ${person1.age}")
 
     val person2 = Person(name = "Alex", age = 86)
-    person.action()
+
     println("Name : ${person2.name} and Age: ${person2.age}")
 
-    val person3 = Person( _email = "abc@gmail.com","Alex", 26)
-    println("Name ${person3.name} and Age: ${person3.age} and email is ${person3.email}")
+    val person3 = Person(_email = "abc@gmail.com", "Alex", 26)
+    println("Name ${person3.name} and Age: ${person3.age} and email is ${person3.email} and the name length is ${person2.nameLength}")
 }
