7d8ebf7f32c11383ba8eda4283d265f1f0364e51
parsaM110 <pmomeni80@gmail.com>
2023-09-17 15:21:27 +0330

    'inheritence'

--- src/main/kotlin/com/example/gitircource/classes/Inheritance.kt
+++ src/main/kotlin/com/example/gitircource/classes/Inheritance.kt
@@ -1,14 +1,21 @@
 package com.example.gitircource.classes
 
 open class User(val name: String) {
+    open var isLoggedIn: Boolean = false
 
-    fun login(){
+    open fun login() {
         println("inside user Login: $name")
     }
 }
 
 
-class Student(name: String) : User(name)
+class Student(name: String) : User(name) {
+    override var isLoggedIn: Boolean = false
+    override fun login() {
+        println("inside student Login: $name") // this is the override thing
+        super.login()
+    }
+}
 
 class Instructor(name: String) : User(name)
 
@@ -17,6 +24,8 @@ fun main() {
     val student = Student("Alex")
     println("name is ${student.name}")
     student.login()
+    student.isLoggedIn = true
+    println("Logged in values is : ${student.isLoggedIn}")
     val instructor = Instructor("Dilip")
     println("name is ${instructor.name}")
     instructor.login()
