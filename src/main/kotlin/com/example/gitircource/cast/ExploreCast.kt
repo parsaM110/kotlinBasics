3018f2f804a4ea2b863078cffb2126cfebd664ba
parsaM110 <pmomeni80@gmail.com>
2023-09-23 16:27:12 +0330

    'type check and casting '

---
+++ src/main/kotlin/com/example/gitircource/cast/ExploreCast.kt
@@ -0,0 +1,43 @@
+package com.example.gitircource.cast
+
+import com.example.gitircource.classes.Course
+
+fun checkType(type: Any) {
+
+    when (type) {
+
+        is Course -> {
+            //val course = type as Course
+            println(type.copy())
+        }
+
+        is String -> {
+            println(type.lowercase())
+        }
+
+    }
+
+}
+
+fun castNumber(any: Any) {
+    when(any) {
+        any as Double -> println("Value is Double")
+    }
+}
+
+fun main() {
+    val course = Course(1, "Reactive Programming in Modern Java using Project Reactor", "Dilip")
+    checkType(course)
+    checkType("Dilip")
+
+    castNumber(1.0)
+   // castNumber(1)
+
+    val number = 1
+    val numberDouble = number.toDouble()
+    println(numberDouble)
+}
+
+
+
+
