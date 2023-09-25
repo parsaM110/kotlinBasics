4b9c8a5c7d1cf366abe0f2278eb7acf5c90afb2a
parsaM110 <pmomeni80@gmail.com>
2023-09-25 22:07:38 +0330

    'make it parameterize and not hard coded by lambda predicate and invoke    '

--- src/main/kotlin/com/example/gitircource/collections/CollectionOperations.kt
+++ src/main/kotlin/com/example/gitircource/collections/CollectionOperations.kt
@@ -6,13 +6,18 @@ import com.example.gitircource.dataset.courseList
 
 fun main() {
     val courseList = courseList()
-    explorefilter(courseList)
+    val devPredicate = { c: Course -> c.category == CourseCategory.DEVELOPEMENT }
+    explorefilter(courseList, devPredicate)
 }
 
-fun explorefilter(courseList: MutableList<Course>) {
+fun explorefilter(
+    courseList: MutableList<Course>,
+    predicate: (Course) -> Boolean
+) {
 
     val developmentCourses = courseList
-        .filter { it.category == CourseCategory.DEVELOPEMENT }
+//        .filter { it.category == CourseCategory.DEVELOPEMENT }
+        .filter { predicate.invoke(it) }
         .forEach { println("developmentCourses : $it") }
 
 //    println("developmentCourses : $developmentCourses")
