97eb4e033f69295e45c5e6c0d7ce536330f9f16d
parsaM110 <pmomeni80@gmail.com>
2023-09-25 22:45:40 +0330

    '  using of flatMap '

--- src/main/kotlin/com/example/gitircource/collections/CollectionOperations.kt
+++ src/main/kotlin/com/example/gitircource/collections/CollectionOperations.kt
@@ -2,26 +2,64 @@ package com.example.gitircource.collections
 
 import com.example.gitircource.dataset.Course
 import com.example.gitircource.dataset.CourseCategory
+import com.example.gitircource.dataset.KAFKA
 import com.example.gitircource.dataset.courseList
 
 fun main() {
     val courseList = courseList()
+
     val devPredicate = { c: Course -> c.category == CourseCategory.DEVELOPEMENT }
     val desPredicate = { c: Course -> c.category == CourseCategory.DESIGN }
-//    explorefilter(courseList, desPredicate)
 
-    exploreMap(courseList,desPredicate)
+    val list = listOf(listOf(1, 2, 3), listOf(4, 5, 6))
+
+    val mapResult = list.map { outerList ->
+        outerList.map {
+            it.toDouble()
+        }
+    }
+
+    println("mapResult: $mapResult")
+
+    val flatMapResult = list.flatMap { outerList ->
+        outerList.map {
+            it.toDouble()
+        }
+    }
+
+    println("flatMapResult: $flatMapResult")
+
+
+    //    exploreFilter(courseList, desPredicate)
+
+    //   exploreMap(courseList,desPredicate)
+
+    val courses = exploreFlatMap(courseList, KAFKA)
+    println("kafka courses : $courses")
+
+}
+
+fun exploreFlatMap(courseList: MutableList<Course>, kafka: String): List<String> {
+    val kafkaCourses = courseList.flatMap { course ->
+        val courseName = course.name
+        course.topicsCovered.filter {
+            it == kafka
+        }.map {
+            courseName
+        }
+    }
+    return kafkaCourses
 }
 
 fun exploreMap(courseList: MutableList<Course>, predicate: (Course) -> Boolean) {
     val courses = courseList
         .filter(predicate) // no need to invoke when you use parentheses
-        .map{ "${it.name} - ${it.category}"}
-        .forEach{println(it)}
+        .map { "${it.name} - ${it.category}" }
+        .forEach { println(it) }
 //    println(courses)
 }
 
-fun explorefilter(
+fun exploreFilter(
     courseList: MutableList<Course>,
     predicate: (Course) -> Boolean
 ) {
