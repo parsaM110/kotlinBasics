0cea7930d5a7f1a8bf062b331da26ec27686b749
parsaM110 <pmomeni80@gmail.com>
2023-09-25 20:08:36 +0330

    'High order lambda'

--- src/main/kotlin/com/example/gitircource/collections/ExploreLamdas.kt
+++ src/main/kotlin/com/example/gitircource/collections/ExploreLamdas.kt
@@ -1,5 +1,18 @@
 package com.example.gitircource.collections
 
+
+/*
+* Higher Order Function    -> block comment : cmd + alt + /
+* */
+
+// simple comment : cmd + /
+
+fun calculate(x: Int, y: Int, op: (x: Int, y: Int) -> Int): Int {
+
+    return op(x, y)
+
+}
+
 fun main() {
     val addLambda = { x: Int -> x + x }
 
@@ -13,5 +26,12 @@ fun main() {
 
     val multiplyResult = multiplyLambda(2, 4)
     println("multiplyResult $multiplyResult")
+
+//    val result = calculate(2, 3, { x, y -> x * y })
+    val result = calculate(2, 3) { x, y -> x * y } // better approach
+    println("result :$result")
+
+    val add = calculate(2, 3, { x, y -> x + y })
+    println("result :$add")
 
 }
