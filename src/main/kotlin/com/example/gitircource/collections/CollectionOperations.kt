7508adbe7fc90f7677eadef4aa6444958345a15d
parsaM110 <pmomeni80@gmail.com>
2023-09-25 22:09:05 +0330

    'changing and using design category '

--- src/main/kotlin/com/example/gitircource/collections/CollectionOperations.kt
+++ src/main/kotlin/com/example/gitircource/collections/CollectionOperations.kt
@@ -7,7 +7,8 @@ import com.example.gitircource.dataset.courseList
 fun main() {
     val courseList = courseList()
     val devPredicate = { c: Course -> c.category == CourseCategory.DEVELOPEMENT }
-    explorefilter(courseList, devPredicate)
+    val desPredicate = { c: Course -> c.category == CourseCategory.DESIGN }
+    explorefilter(courseList, desPredicate)
 }
 
 fun explorefilter(
@@ -18,7 +19,7 @@ fun explorefilter(
     val developmentCourses = courseList
 //        .filter { it.category == CourseCategory.DEVELOPEMENT }
         .filter { predicate.invoke(it) }
-        .forEach { println("developmentCourses : $it") }
+        .forEach { println("Courses : $it") }
 
 //    println("developmentCourses : $developmentCourses")
 
