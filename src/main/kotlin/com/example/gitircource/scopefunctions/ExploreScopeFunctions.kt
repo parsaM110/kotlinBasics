16fa93e7ce8a9c0df5be863380e9250159ad3c36
parsaM110 <pmomeni80@gmail.com>
2023-09-26 22:16:52 +0330

    ' run and with scope fun (not extension fun) '

--- src/main/kotlin/com/example/gitircource/scopefunctions/ExploreScopeFunctions.kt
+++ src/main/kotlin/com/example/gitircource/scopefunctions/ExploreScopeFunctions.kt
@@ -6,7 +6,46 @@ import com.example.gitircource.classes.CourseCategory
 fun main() {
    // exploreApply()
 //    exploreAlso()
-    exploreLet()
+//    exploreLet()
+//    exploreWith()
+    exploreRun()
+}
+
+fun exploreRun() {
+
+    var numbers : MutableList<Int> ? = null
+    val result = numbers.run{
+        numbers = mutableListOf(1, 2, 3)
+        numbers?.sum()
+    }
+
+    println("Run Result is : $result")
+
+    val lenght = run{
+        val name  = "Dilip"
+        println(name)
+        name.length
+
+    }
+
+    println("Run lenght is : $lenght")
+
+ }
+
+
+fun exploreWith() {
+    val numbers = mutableListOf(1,2,3,4,5)
+    val result = with(numbers){
+       /* println("Size is ${numbers.size}")
+        val list = numbers.plus(6)
+        list.sum()*/
+        println("Size is $size")
+        val list = plus(6)
+        list.sum()
+
+    }
+
+    println("With result: $result")
 }
 
 fun exploreLet() {
